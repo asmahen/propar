cbdc2b7b8709939de58cd0e05eed40ddd69f4225
Hugo Roger <hugoroger.hr@gmail.com>
2022-04-20 14:09:37 +0200

    ajout du CSS sur la page 'log in' et sur la page 'ajout d'un colla'

--- migrations/Version20220420090904.php
+++ migrations/Version20220420090904.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220420080120 extends AbstractMigration
+final class Version20220420090904 extends AbstractMigration
 {
     public function getDescription(): string
     {
