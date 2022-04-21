47d6bf9316e67320ccf8be2df6d3817d0a66798e
Asmahen Cherigui <asmahenc@gmail.com>
2022-04-21 15:14:28 +0200

    bugFix path de logout

--- migrations/Version20220421130428.php
+++ migrations/Version20220421130428.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220420090904 extends AbstractMigration
+final class Version20220421130428 extends AbstractMigration
 {
     public function getDescription(): string
     {
