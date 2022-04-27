16be23fd20cca76ed41b91626e44050b6d218ff6
Valentin Cadart <cadart.valentin@sfr.fr>
2022-04-27 06:25:05 +0200

    ajout du bundle encore, navbar.css config with encore, ajout Chart.js, création de la fonction index avec charts js, requete sql dans operations repository, affichage du graphique

--- src/Controller/ChiffreAffairesController.php
+++ src/Controller/ChiffreAffairesController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Repository\OperationsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
     /**
     * @isGranted("ROLE_EXPERT", message="Vous n'avez pas accès à cette session")
@@ -15,10 +18,50 @@ class ChiffreAffairesController extends AbstractController
     /**
     * @Route("/chiffreAffaires", name="app_chiffre_affaires")
     */
-    public function index(): Response
+    public function index(ChartBuilderInterface $chartBuilder, OperationsRepository $operationsRepository) : Response
     {
+
+        $petite = $operationsRepository->findPetiteOperation();
+        $sommePetite = 0;
+        foreach ($petite as $value) {
+        $sommePetite= $sommePetite + array_sum($petite[0]);
+        }
+        $moyenne = $operationsRepository->findMoyenneOperation();
+        $sommeMoyenne = 0;
+        foreach ($moyenne as $value) {
+            $sommeMoyenne= $sommeMoyenne + array_sum($moyenne[0]);
+        }
+
+        $grande = $operationsRepository->findGrandeOperation();
+        $sommeGrande = 0;
+        foreach ($grande as $value) {
+            $sommeGrande= $sommeGrande + array_sum($grande[0]);
+        }
+
+
+        $chart = $chartBuilder->createChart(Chart::TYPE_PIE);
+        $chart->setData([
+            'labels' => ['Petites Opérations', 'Moyennes Opérations', 'Grandes Opérations'],
+            'datasets' => [
+                [
+                    'label' => 'Points',
+                    'backgroundColor' => ['#f1c40f','#e67e22', '#16a085'],
+                    'borderColor' => 'black',
+                    'data' => [$sommePetite, $sommeMoyenne, $sommeGrande],
+                    'hoverOffset' => 4,
+                    'borderWidth' => 2,
+
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'animation.animateScale'=>true,
+
+        ]);
+
         return $this->render('chiffre_affaires/index.html.twig', [
-            'controller_name' => 'ChiffreAffairesController',
+            'chart' => $chart,
         ]);
     }
 }
